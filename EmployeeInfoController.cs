22f123b20d53f47c43aa433f2fdd6dbe9a13fc67
Yogesh Mandora <82234408+Yogeshmandora0786@users.noreply.github.com>
2023-03-13 22:41:37 +0530

    Add files via upload

---
+++ EmployeeInfoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OperationDomian.Entities;
+using OperationRepository.Repositories;
+
+namespace ShreeGaneshWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeInfoController : Controller
+    {
+        EmployeeInfoRepository objemployee = new EmployeeInfoRepository();
+
+        [HttpGet]
+        [Route("Get")]
+        public IEnumerable<EmployeeInfo> Get()
+        {
+            return objemployee.GetEntity();
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public int Add(EmployeeInfo employee)
+        {
+            return objemployee.AddEntity(employee);
+        }
+
+        [HttpPut]
+        [Route("Update")]
+        public int Update(EmployeeInfo employee)
+        {
+            return objemployee.UpdateEntity(employee);
+        }
+
+        [HttpDelete]
+        [Route("Delete/{id}")]
+        public int Delete(int id)
+        {
+            return objemployee.DeleteEntity(id);
+        }
+    }
+}
